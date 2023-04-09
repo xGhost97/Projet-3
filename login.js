26239d71e4b091aa92b9e110ff3f4e9338f79df7
Pierluigi Sophie <pierluigisophie@gmail.com>
2023-04-09 23:58:15 +0200

    Modification

--- login.js
+++ login.js
@@ -1,16 +1,35 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // empêcher la soumission par défaut du formulaire
+const myForm = document.querySelector("#myForm");
+myForm.addEventListener("submit", function(event) { 
+    event.preventDefault(); 
 
-    // récupérer les données du formulaire
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+    const email = event.target.querySelector("[name=email]").value;
+    const password = event.target.querySelector("[name=password]").value;
 
-    // vérifier les données utilisateur et mot de passe
-    if (email === "utilisateur" && password === "motdepasse") {
-        // rediriger l'utilisateur vers la page d'accueil
-        window.location.href = "page-d-accueil.html";
-    } else {
-        // afficher un message d'erreur
-        document.getElementById("errorMessage").style.display = "block";
-    }
+    fetch ("http://localhost:5678/api/users/login", { 
+        method : "POST",
+        headers:{ 
+            Authorization : "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+    })
+    // Le message d'erreur qui doit s'afficher
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error("Erreur dans l'identifiant ou le mot de passe");
+        }
+        return res.json();
+    })
+    .then((data) => {
+        // Rediriger vers la page du site avec des boutons d'actions pour éditer le site
+        window.location.href = "index.html";
+    })
+    .catch((error) => {
+        // Afficher le message d'erreur
+        console.error(error);
+        alert(error.message);
+    });
 });
--- script.js
+++ script.js
@@ -77,3 +77,18 @@ let allButton = document.createElement("button");
          
 }
 }
+
+
+// CREATION DE LA MODAL
+
+const openModal = function(e) {
+  e.preventDefault()
+  const target = document.querySelector(e.target.getAttribute("href"))
+  target.style.display = null
+  target.removeAttribute("aria-hidden")
+  target.setAttribute("aria-modal", "true")
+}
+
+document.querySelectorAll(".js-modal").forEach(a => {
+  a.addEventListener("click", openModal)
+})
